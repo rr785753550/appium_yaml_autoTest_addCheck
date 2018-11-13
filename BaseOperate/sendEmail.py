2fa4cb007b3a8dc8bc863ca7720f5c00c3385696
rr785753550 <1622852733@qq.com>
2018-11-13 18:18:55 +0800

    Update sendEmail.py

--- BaseOperate/sendEmail.py
+++ BaseOperate/sendEmail.py
@@ -4,62 +4,89 @@ import smtplib
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import configparser
 
 
-# 定义发件人和收件人
-mail_from = "XXXX"  # 发件人地址
-mail_password = "XXXX"  # 密码
-# receiverPath = "F:\\PythonWorkSpace\\autoTest\common\\emailReceiver\\receiverAddr.txt"
-receiverPath = os.path.join(os.getcwd(), 'common/emailReceiver/receiverAddr.txt')
-fp = open(receiverPath, 'r')
-content = fp.readlines()
-for i in range(len(content)):
-    content[i] = content[i][:len(content[i])-1]
-fp.close()
-mail_to = content
+class Email:
+    def __init__(self):
+        PATH = lambda p: os.path.abspath(
+            os.path.join(os.path.dirname(__file__), p))
+        confPath = PATH('../common/emailAddr/emailAddr.txt')
+        self.conf = configparser.ConfigParser()
+        self.conf.read(confPath, encoding='utf-8')
 
+    def email_sender(self):
+        emailSender = {}
+        senderName = self.conf.get("emailSender", "name")
+        senderPsw = self.conf.get("emailSender", "password")
+        emailSender['name'] = senderName
+        emailSender['password'] = senderPsw
+        # print(emailSender)
+        return emailSender
 
-def email_content():
-    # 邮件主题
-    message = MIMEMultipart()
-    message['From'] = Header(u"测试" + "<" + mail_from + ">", 'utf-8')
-    message['To'] = ";".join(mail_to)
-    message['Subject'] = Header(u"自动化测试报告", 'utf-8')
+    def email_receiver(self):
+        emailReceiver = []
+        allKeys = self.conf.options('emailReceiver')
+        for key in allKeys:
+            temp = self.conf.get('emailReceiver', key)
+            emailReceiver.append(temp)
+        # print(emailAddr)
+        return emailReceiver
 
-    # 获取最新的excel报告
-    # report_html_dir = "F:\\PythonWorkSpace\\autoTest\\report\\report"
-    report_dir = os.path.join(os.getcwd(), 'results/report/')
-    report_lists = os.listdir(report_dir)
-    report_lists = sorted(report_lists)  # 排序：升序
-    if len(report_lists) == 0:
-        return None
-    print("最新的文件为：" + report_lists[-1])
-    report_File = os.path.join(report_dir, report_lists[-1])
+    def email_content(self):
+        # 邮件主题
+        senderName = self.email_sender()['name']
+        emailReceiver = self.email_receiver()
+        message = MIMEMultipart()
+        message['From'] = Header(u"测试" + "<" + senderName + ">", 'utf-8')
+        message['To'] = ";".join(emailReceiver)
+        message['Subject'] = Header(u"自动化测试报告", 'utf-8')
 
-    # # 邮件内容：
-    emailContent1 = MIMEText("附件为自动化测试报告，请查收", 'plain', 'utf-8')
-    message.attach(emailContent1)
-
-    # 添加邮件附件1:report.xlsx
-    enclosure1 = MIMEText(open(report_File, 'rb').read(), 'base64', 'utf-8')
-    enclosure1["Content-Type"] = 'application/octet-stream'
-    enclosure1["Content-Disposition"] = 'attachment; filename="report.xlsx"'
-    message.attach(enclosure1)
-
-    return message
-
-
-def sendreport():
-    # 发送邮件
-    try:
-        smtp = smtplib.SMTP_SSL("smtp.exmail.qq.com", 465)
-        smtp.login(mail_from, mail_password)
-        message = email_content()
-        if message is None:
+        # 获取最新的excel报告
+        PATH = lambda p: os.path.abspath(
+            os.path.join(os.path.dirname(__file__), p))
+        report_dir = PATH('../results/report/')
+        # report_dir = os.path.join(os.getcwd(), 'results/report/')
+        report_lists = os.listdir(report_dir)
+        report_lists = sorted(report_lists)  # 排序：升序
+        if len(report_lists) == 0:
             return None
-        smtp.sendmail(mail_from, mail_to, message.as_string())
-        smtp.quit()
-        print("email has send out!")
-    except smtplib.SMTPException:
-        print("email send error!")
+        print("最新的文件为：" + report_lists[-1])
+        report_File = os.path.join(report_dir, report_lists[-1])
 
+        # 邮件内容：
+        # emailContent1 = MIMEText("附件为自动化测试报告，请查收", 'plain', 'utf-8')
+        emailContent1 = MIMEText(open(report_File, 'rb').read(), 'plain', 'utf-8')
+        message.attach(emailContent1)
+        # 添加邮件附件1:report.xlsx
+        enclosure1 = MIMEText(open(report_File, 'rb').read(), 'base64', 'utf-8')
+        enclosure1["Content-Type"] = 'application/octet-stream'
+        enclosure1["Content-Disposition"] = 'attachment; filename="report.xlsx"'
+        message.attach(enclosure1)
+
+        return message
+
+    def sendreport(self):
+        # 发送邮件
+        emailSender = self.email_sender()
+        senderName = emailSender['name']
+        senderPsw = emailSender['password']
+        emailReceiver = self.email_receiver()
+        try:
+            smtp = smtplib.SMTP_SSL("smtp.exmail.qq.com", 465)
+            smtp.login(senderName, senderPsw)
+            message = self.email_content()
+            if message is None:
+                return None
+            smtp.sendmail(senderName, emailReceiver, message.as_string())
+            smtp.quit()
+            print("email has send out!")
+        except smtplib.SMTPException:
+            print("email send error!")
+
+
+# if __name__ == '__main__':
+    # Email().email_sender()
+    # Email().email_sender()
+    # Email().email_content()
+    # Email().sendreport()
